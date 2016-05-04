2d13d80cf7a8b328c72b9988b6aed372f7bee396
Edgardo Pinto-Escalier Scoffe <ep-marketing-media@live.com>
2016-05-04 19:15:10 +0200

    Added main function to script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+var main = function() {
+        $('.day').click(function() {
+            $(this).next().toggle();
+            $(this).find('.weekday span').toggleClass('glyphicon-minus');
+        });
+    };
+    $(document).ready(main);
